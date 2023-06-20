387662154c63af8742e415f600a0de447bdf26d1
WDsayeed <sayeedahammed526@gmail.comm>
2023-06-20 21:41:44 +0600

    added vercel

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvg3x0p.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -17,76 +16,69 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-async function run() {
+const dbConnect = async () => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const usersCollection = client.db('usersManagement').collection('users')
-
-    app.post('/users', async(req, res)=>{
-        const user = req.body 
-        const query = { email: user.email}
-        const existUser = await usersCollection.findOne(query)
-        if(existUser){
-                return res.send({message: 'user already exists'})
-        }
-        const result = await usersCollection.insertOne(user)
-        res.send(result)
-    })
-
-    app.get('/users', async(req, res)=>{
-      const result = await usersCollection.find().toArray()
-      res.send(result)
-    })
-
-    app.get('/users/:id', async(req, res)=>{
-      const id = req.params.id
-      console.log(id)
-      const query = {_id: new ObjectId(id)}
-      const result = await usersCollection.findOne(query)
-      res.send(result)
-    })
-
-    app.put('/usersUpdate/:id', async(req, res)=>{
-      const id = req.params.id 
-      const query = {_id: new ObjectId(id)}
-      const updatedUser = req.body
-      const user = {
-        $set:{
-          name:updatedUser.name,
-          email:updatedUser.email,
-          phone:updatedUser.phone
-        }
-      }
-      const result = await usersCollection.updateOne(query, user)
-      res.send(result)
-    })
-
-    app.delete('/usersDelete/:id', async(req, res)=>{
-      const id = req.params.id 
-      const query = {_id: new ObjectId(id)}
-      const result = await usersCollection.deleteOne(query)
-      res.send(result)
-    })
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-//     await client.close();
+      await client.connect()
+      console.log('Database Connected!')
+  } catch (error) {
+      console.log(error.name, error.message)
   }
 }
-run().catch(console.dir);
+dbConnect()
 
+const usersCollection = client.db("usersManagement").collection("users");
+app.get("/", (req, res) => {
+  res.send("users are connected");
+});
 
-app.get('/', (req, res)=>{
-        res.send('users are connected')
-})
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  const query = { email: user.email };
+  const existUser = await usersCollection.findOne(query);
+  if (existUser) {
+    return res.send({ message: "user already exists" });
+  }
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
 
-app.listen(port, ()=>{
-        console.log(`users are connected on port ${port}`)
-})
+app.get("/users", async (req, res) => {
+  const result = await usersCollection.find().toArray();
+  res.send(result);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await usersCollection.findOne(query);
+  res.send(result);
+});
+
+app.put("/usersUpdate/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const updatedUser = req.body;
+  const user = {
+    $set: {
+      name: updatedUser.name,
+      email: updatedUser.email,
+      phone: updatedUser.phone,
+    },
+  };
+  const result = await usersCollection.updateOne(query, user);
+  res.send(result);
+});
+
+app.delete("/usersDelete/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await usersCollection.deleteOne(query);
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`users are connected on port ${port}`);
+});
